ededcec188d5cac1afeee36b40ee6ac053bb7d8c
Muslih Aqqad <muslih@users.noreply.github.com>
2022-06-23 19:18:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let string = "";
 
 for (let i = 1; i <= n; i++){
     for (let j = 1; j <= n - i; j++){
-        string += "";
+        string += " ";
     }
     for (let k = 0; k < 2 * i - 1; k++){
         string += "*";
